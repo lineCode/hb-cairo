e15a5dd4f0c773ff64a78ac55c42bc11d5fa4397
Rafał Jopek <rafaljopek@hotmail.com>
2020-11-13 01:42:41 +0100

    Update pdf.c
    Functions as documented

--- src/pdf.c
+++ src/pdf.c
@@ -8,6 +8,11 @@
 #include "hbcairo.h"
 #include "cairo-pdf.h"
 
+/*
+ * enum                cairo_pdf_version_t;
+ */
+
+// cairo_surface_t * cairo_pdf_surface_create( const char *filename, double width_in_points, double height_in_points );
 HB_FUNC( CAIRO_PDF_SURFACE_CREATE )
 {
 #ifdef CAIRO_HAS_PDF_SURFACE
@@ -17,6 +22,15 @@ HB_FUNC( CAIRO_PDF_SURFACE_CREATE )
 #endif
 }
 
+// cairo_surface_t * cairo_pdf_surface_create_for_stream( cairo_write_func_t write_func, void *closure, double width_in_points, double height_in_points );
+
+// void cairo_pdf_surface_restrict_to_version( cairo_surface_t *surface, cairo_pdf_version_t version );
+
+// void cairo_pdf_get_versions( cairo_pdf_version_t const **versions, int *num_versions );
+
+// const char * cairo_pdf_version_to_string( cairo_pdf_version_t version );
+
+// void cairo_pdf_surface_set_size( cairo_surface_t *surface, double width_in_points, double height_in_points );
 HB_FUNC( CAIRO_PDF_SURFACE_SET_SIZE )
 {
 #ifdef CAIRO_HAS_PDF_SURFACE
