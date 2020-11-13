fde2690c04f2f48ecba287362be0c48011889d1c
Rafał Jopek <rafaljopek@hotmail.com>
2020-11-13 01:36:47 +0100

    Update svg.c
    Functions as documented

--- src/svg.c
+++ src/svg.c
@@ -7,6 +7,11 @@
 
 #include "hbcairo.h"
 
+/*
+ * enum cairo_svg_version_t; 
+ */
+
+// cairo_surface_t * cairo_svg_surface_create( const char *filename, double width_in_points, double height_in_points );
 HB_FUNC( CAIRO_SVG_SURFACE_CREATE )
 {
 #ifdef CAIRO_HAS_SVG_SURFACE
@@ -15,3 +20,11 @@ HB_FUNC( CAIRO_SVG_SURFACE_CREATE )
    hb_retptr( NULL );
 #endif
 }
+
+// cairo_surface_t * cairo_svg_surface_create_for_stream( cairo_write_func_t write_func, void *closure, double width_in_points, double height_in_points );
+
+// void cairo_svg_surface_restrict_to_version( cairo_surface_t *surface, cairo_svg_version_t version );
+
+// void cairo_svg_get_versions( cairo_svg_version_t const **versions, int *num_versions );
+
+// const char * cairo_svg_version_to_string( cairo_svg_version_t version );
