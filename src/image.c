5a6e3feb07dcdd8f7903a04501a6a5f49af67075
Rafał Jopek <rafaljopek@hotmail.com>
2020-11-20 13:29:37 +0100

    Update image.c for Cairo 1.16.0

--- src/image.c
+++ src/image.c
@@ -5,10 +5,21 @@
  *
  * Copyright 2009 Mindaugas Kavaliauskas <dbtopas at dbtopas.lt>
  *
+ * Image Surfaces
+ * Image Surfaces — Rendering to memory buffers
+ *
+ * Types and Values
+ *
+ * #define CAIRO_HAS_IMAGE_SURFACE
+ * enum    cairo_format_t
+ *
  */
 
 #include "hbcairo.h"
 
+// int cairo_format_stride_for_width( cairo_format_t format, int width );
+
+// cairo_surface_t* cairo_image_surface_create( cairo_format_t format, int width, int height );
 HB_FUNC( CAIRO_IMAGE_SURFACE_CREATE )
 {
 #ifdef CAIRO_HAS_IMAGE_SURFACE
@@ -18,6 +29,7 @@ HB_FUNC( CAIRO_IMAGE_SURFACE_CREATE )
 #endif
 }
 
+// cairo_surface_t* cairo_image_surface_create_for_data( unsigned char *data, cairo_format_t format, int width, int height, int stride );
 HB_FUNC( CAIRO_IMAGE_SURFACE_CREATE_FOR_DATA )
 {
 #ifdef CAIRO_HAS_IMAGE_SURFACE
@@ -26,3 +38,13 @@ HB_FUNC( CAIRO_IMAGE_SURFACE_CREATE_FOR_DATA )
    hb_retptr( NULL );
 #endif
 }
+
+// unsigned char* cairo_image_surface_get_data( cairo_surface_t *surface );
+
+// cairo_format_t cairo_image_surface_get_format( cairo_surface_t *surface );
+
+// int cairo_image_surface_get_width( cairo_surface_t *surface );
+
+// int cairo_image_surface_get_height( cairo_surface_t *surface );
+
+// int cairo_image_surface_get_stride( cairo_surface_t *surface );
