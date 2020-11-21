9454752c04edde65ce83d4f2b75e8d69b42d48cb
Rafał Jopek <rafaljopek@hotmail.com>
2020-11-21 20:19:24 +0100

    Update svg-01.c

--- examples/c/svg/svg-01.c
+++ examples/c/svg/svg-01.c
@@ -5,32 +5,16 @@
 
 int main( void )
 {
-   cairo_surface_t *surface = cairo_svg_surface_create( "svg.svg", 500.0, 500.0 );
+   cairo_surface_t *surface = cairo_svg_surface_create( "svg-01.svg", 350.0, 100.0 );
    cairo_t *cr = cairo_create( surface );
 
-   /* Draw the squares in the background */
-   for ( int x = 0; x < 10; x++ )
-      for ( int y = 0; y < 10; y++ )
-         cairo_rectangle( cr, x * 10.0, y * 10.0, 5, 5 );
+   cairo_set_source_rgb( cr, 0, 0, 0 );
+   cairo_select_font_face( cr, "Courier New", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL );
+   cairo_set_font_size( cr, 30 );
 
-   cairo_pattern_t *pattern = cairo_pattern_create_radial( 50, 50, 5, 50, 50, 50 );
-   cairo_pattern_add_color_stop_rgb( pattern, 0, 0.75, 0.15, 0.99 );
-   cairo_pattern_add_color_stop_rgb( pattern, 0.9, 1, 1, 1 );
+   cairo_move_to( cr, 10, 50 );
+   cairo_show_text( cr, "Learning Harbour..." );
 
-   cairo_set_source( cr, pattern );
-   cairo_fill( cr );
-
-   /* Writing in the foreground */
-   cairo_set_font_size( cr, 15 );
-   cairo_select_font_face( cr, "Georgia", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD );
-   cairo_set_source_rgb ( cr, 0, 0, 0 );
-
-   cairo_move_to( cr, 10, 25 );
-   cairo_show_text( cr, "Hallo" );
-
-   cairo_move_to( cr, 10, 75 );
-   cairo_show_text( cr, "Harbour" );
-
-   cairo_destroy( cr );
    cairo_surface_destroy( surface );
+   cairo_destroy( cr );
 }
